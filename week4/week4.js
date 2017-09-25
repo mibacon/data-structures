02c6f2dd49df2fa5eff9225d8cda3325c6472e96
michal <hush.some1@gmail.com>
2017-09-25 16:23:17 +0000

    week4 added

---
+++ week4/week4.js
@@ -0,0 +1,37 @@
+// npm install mongodb
+var fs = require('fs');
+var request = require('request');
+
+// IN MONGO exists a database `citibike` with a collection `stations`
+var dbName = 'AAMeetings'; // name of Mongo database (created in the Mongo shell)
+var collName = 'meetings'; // name of Mongo collection (created in the Mongo shell)
+
+// Request the JSON data on citibike stations
+// Insert the list of citibike stations (contained in an array) in the Mongo collection
+
+var addresses = fs.readFileSync('/home/ubuntu/workspace/week3/output.txt')
+var parsed = JSON.parse(addresses)
+
+// console.log(parsed[0].formatAddress)
+request(parsed, function(error, response, body) { 
+    // var meetingData = JSON.parse(body);
+    // console.log(dbName)
+
+    // Connection URL
+    var url = 'mongodb://' + process.env.IP + ':27017/' + dbName;
+
+    // // Retrieve
+    var MongoClient = require('mongodb').MongoClient; 
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) {return console.dir(err);}
+
+        var collection = db.collection(collName);
+
+    //     // THIS IS WHERE THE DOCUMENT(S) IS/ARE INSERTED TO MONGO:
+        collection.insert(parsed);
+        db.close();
+
+    }); //MongoClient.connect
+
+}); //request
